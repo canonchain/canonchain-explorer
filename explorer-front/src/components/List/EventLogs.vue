a01f9c45485397e15c420d8d961226cd66dc9029
zhuanbang <79228807@qq.com>
2019-12-05 23:48:26 +0800

    Update Ui

--- explorer-front/src/components/List/EventLogs.vue
+++ explorer-front/src/components/List/EventLogs.vue
@@ -3,23 +3,23 @@
         <!-- start -->
         <div v-for="(item,index) in database" :key="index">
             <div class="bui-dlist token-item">
-                <div class="block-item-des">
+                <!-- <div class="block-item-des">
                     <strong class="bui-dlist-tit">Address</strong>
                     <div class="bui-dlist-det">{{ item.address }}</div>
-                </div>
+                </div> -->
                 <div class="block-item-des">
                     <strong class="bui-dlist-tit">Topic</strong>
                     <div class="bui-dlist-det">
                         <template v-for="(child_item, index) in item.topics">
                             [{{ index }}]
-                            {{ child_item }}
+                            {{ child_item.toUpperCase() }}
                             <br>
                         </template>
                     </div>
                 </div>
                 <div class="block-item-des">
                     <strong class="bui-dlist-tit">Data</strong>
-                    <div class="bui-dlist-det">{{ item.data }}</div>
+                    <div class="bui-dlist-det">{{ item.data || "-" }}</div>
                 </div>
             </div>
         </div>
